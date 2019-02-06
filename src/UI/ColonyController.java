8f5a06054d5fb60c264c76ad45af5dec9419dc42
Valchanov <valchano@egb070000053.lancs.local>
2019-02-06 19:38:52 +0000

    added comment to the colony controller as well as offloaded the iteration to a sperarate thread that asynchronously renders the colony when done iterating

--- src/UI/ColonyController.java
+++ src/UI/ColonyController.java
@@ -2,12 +2,18 @@ package UI;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Cursor;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Slider;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -20,31 +26,62 @@ import java.nio.file.Paths;
 
 import static java.util.Objects.*;
 
-
-public class ColonyController {
+/**
+ * Represents an MVC controller that controls the a Colony model and visualizes it with a Colony view object.
+ */
+public class ColonyController extends Service<Colony> {
 
     private Colony colony;
     private ColonyView colonyView;
     private Timeline timeline;
     private Stage stage;
     private FileChooser fileChooser;
+    private Task<Colony> colonyTask;
+    private Alert alert;
 
     @FXML
     private Slider speed = null;
 
+    /**
+     * Creates a new controller object. This constructors is to be used only when the controllers is parsed to a FXML file.
+     */
     public ColonyController() {
         fileChooser = new FileChooser();
         fileChooser.setTitle("Select colony file");
         fileChooser.getExtensionFilters().add(
                 new FileChooser.ExtensionFilter("CSV", "*.csv")
         );
+
+        alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error reading file");
+        alert.getDialogPane().paddingProperty().setValue(new Insets(10, 20,10, 20));
     }
 
-
-    public ColonyController(Colony colony, ColonyView colonyView) {
+    /**
+     * Creates a new Controller object with the provided view and model.
+     *
+     * @param colonyModel the colony that is to be controlled
+     * @param colonyView  the view object that is going to be used to render the colony
+     */
+    public ColonyController(Stage stage, Colony colonyModel, ColonyView colonyView) {
         this();
-        this.colonyView = colonyView;
-        this.colony = colony;
+       setColony(colonyModel);
+       setStage(stage);
+       setColonyView(colonyView);
+    }
+
+    @Override
+    protected Task<Colony> createTask() {
+        return new Task<Colony>() {
+            @Override
+            protected Colony call() throws Exception {
+                colony.iterate();
+
+                //run on main application thread
+                Platform.runLater(()-> colonyView.render(colony));
+                return colony;
+            }
+        };
     }
 
     @FXML
@@ -52,31 +89,35 @@ public class ColonyController {
         speed.valueProperty().addListener(this::changeSpeed);
     }
 
-    public EventHandler<ActionEvent> frameListener() {
-        return this::iterateColony;
-    }
 
     private void iterateColony(ActionEvent event) {
 
-        colony.iterate();
-        colonyView.render(colony);
-        //todo put on a separate thread
+        if (isNull(colonyTask) || colonyTask.isDone()) {
+            this.colonyTask = this.createTask();
+            this.executeTask(colonyTask);
+        }
+
         event.consume();
     }
 
-
+    /**
+     * Starts the animation of the i
+     *
+     * @param time the duration that each colony generation will be displayed
+     */
     public void startAnimation(Duration time) {
         if (isNull(colony) || isNull(colonyView) || isNull(stage))
-            throw new NullPointerException("Either the colony or the colony view has not been set");
+            throw new NullPointerException("Either the colony, the colony view or the stage has not been set");
 
-        timeline = new Timeline(new KeyFrame(time, frameListener()));
+        timeline = new Timeline(new KeyFrame(time, this::iterateColony));
         timeline.setCycleCount(Timeline.INDEFINITE);
     }
 
     @FXML
     public void play(Event event) {
-        if (nonNull(timeline))
+        if (nonNull(timeline)) {
             timeline.play();
+        }
 
         event.consume();
     }
@@ -117,6 +158,7 @@ public class ColonyController {
 
     public void setColonyView(ColonyView colonyView) {
         this.colonyView = requireNonNull(colonyView);
+        this.colonyView.setCursor(Cursor.OPEN_HAND);
     }
 
     public void setStage(Stage stage) {
@@ -128,13 +170,18 @@ public class ColonyController {
         pause(event);
         try {
             File file = fileChooser.showOpenDialog(stage);
-            Path pathToFile = Paths.get(file.getAbsolutePath());
-            this.colony = Colony.fromCSV(pathToFile);
-            this.colony.iterate();
-            this.colonyView.render(colony);
+            if (nonNull(file)){
+                Path pathToFile = Paths.get(file.getAbsolutePath());
+                this.colony = Colony.fromCSV(pathToFile);
+
+                this.colony.iterate();
+                this.colonyView.render(colony);
+
+            }
         } catch (IOException | IllegalArgumentException e) {
-            //todo handle bad files
-            e.printStackTrace();
+            Text text =  new Text(e.getMessage());
+            alert.getDialogPane().setContent(text);
+            alert.showAndWait();
         }
     }
 
@@ -142,7 +189,12 @@ public class ColonyController {
         if (nonNull(timeline)) {
             timeline.stop();
             timeline.getKeyFrames().clear();
-            timeline.getKeyFrames().add(new KeyFrame(Duration.millis(newValue.intValue()), frameListener()));
+            timeline.getKeyFrames().add(
+                    new KeyFrame(
+                            Duration.millis(newValue.intValue()),
+                            this::iterateColony
+                    )
+            );
             timeline.play();
         }
     }
